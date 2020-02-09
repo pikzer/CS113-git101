94354b33467faf7df5e2f264980cd07c321eb080
pikzer <rujipas.m@ku.th>
2020-02-09 21:21:32 +0700

    HowtoUsePointer

---
+++ Char.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+
+//int main ()
+//{
+    //int *p = 0; //ptr ชี้ไปไหนสักที่
+//}
+//int main() //ขนาดของ ptr เท่ากันเสมอ แม้ว่าจะอ้างอิงอะไร
+//{
+    //char *c;
+    //int *i  ;
+    //float *f;
+    //double *d;
+    //printf("size of(c) = %d\n",sizeof(c));
+    //printf("size of(i) = %d\n",sizeof(i));
+    //printf("size of(f) = %d\n",sizeof(f));
+    //printf("size of(d) = %d\n",sizeof(d));
+//
+//#include<stdio.h>
+//int f1(int i )
+//{
+    //printf("%d\n",i) ;
+//}
+//int main ()
+//{
+    //f1(1+3+4) ; //ต้องตีความในวงเล็บก่อนถึงส่งเข้าฟังก์ชั่น
+//}
+//call by reference
+// #include <stdio.h>
+
+// void squareByValue(int); /* function prototype */
+// void squareByReference(int *); /* function prototype */
+// int main()
+// { int a = 3;
+// int b = 4;
+// printf("คาของ ่ a คอื %d\n", a);
+// squareByValue(a);
+// printf("คาของ ่ a หลงเร ั ยกฟ ี ังกช์ นั squareByValue(a) คอื %d\n\n", a);
+// printf("คาของ ่ b คอื %d\n", b);
+// squareByReference(&b);
+// printf("คาของ ่ b หลงเร ั ยกฟ ี ังกช์ นั squareByReference(&b) คอื %d\n", b);
+// }
+// void squareByValue(int val)
+// {
+// val = val * val;
+// }
+// void squareByReference(int *val)
+// {
+// *val = *val * *val;
+// }
