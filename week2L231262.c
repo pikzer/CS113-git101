7e767e5ae3b4c116c113ea02a01deb8102daf62b
pikzer <rujipas.m@ku.th>
2020-02-09 21:41:57 +0700

    TypeofDATA_Array_Size_IF_ELSE-->Week2

---
+++ week2L231262.c
@@ -0,0 +1,56 @@
+#include <stdio.h>
+#include <stdlib.h>
+//char name[20]; //arrayกำหนดขนาดเสมอ
+//char n[5];
+int main() 
+{
+    //printf ("Enter name : ");
+    //gets (name); //รับ str
+    //fgets (name, 20, stdin); //เตรียมพื้นที่ไว้รอ
+    //printf ("name = %s",n);
+    // printf ("input something : ");
+    //fgets (n,5,stdin);
+    //int i = atoi(n) ;//atoi = array to integer
+    //printf ("n = %d",i);
+    //int i = atoi(n);
+    //double d = atof(n);
+    //long l = atol(n) ;
+    //getchar(c); //รับแค่ตัวแรกที่พิมพ์มา
+//OUTPUT
+    //puts (n);
+    //putchar(100); (//putchar('h') ;)
+//DATA type Boolean
+    //False
+    //int found = 0 ;
+    //float isIn = 0.0;
+    //char isExist = '\0' ;
+    //ที่เหลือจะเป็น True ทั้งหมด
+    //int i = 5;
+//if else syntax  
+    //if (i > 0 && i  < 20){ // && = and
+        //printf("if\n");
+
+    //}else if (i < 100 || !found && isExist){ // || = or , ! = not
+        //print("Else if\n");
+    //}else if (i > 'A' && i <= 'z'){ //เปลี่ยน a เป็น ascii code ไปเทียบ
+        //printf ("else if 2\n");
+
+    //}else {
+        //printf ("else\n");
+    //}
+    //printf("%d\n",i > 0) ; ถ้าเป็นจริง จะปริ้น
+//Loop hwile
+    //int n = [];
+    //fget(n,5,stdin);
+    //int i = 0 ;
+    //int count = 0 ;
+//while (count < i){
+    //printf ("%d\n",count);
+    //count++;
+    //}
+//Loop for  
+    //for (int j = 0; j < i ; j++){  //for (sta1; sta2 ; sta3){
+        //printf("%d\n",j);
+    //}
+    //for (;;) infinite Loop
+}
